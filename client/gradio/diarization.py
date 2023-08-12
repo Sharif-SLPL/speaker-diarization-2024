5edbf5755c9c7867c29855b49b8e8149c324b380
mahdi <mahdifarzadi78@gmail.com>
2023-08-13 02:02:44 +0330

    use asr endpoint in gradio client

--- client/gradio/diarization.py
+++ client/gradio/diarization.py
@@ -1,8 +1,8 @@
 import requests
 
-api_url = 'http://127.0.0.1:8000/diarization/plot'
+api_url = 'http://127.0.0.1:8000/diarization/asr'
 
 
 def diarize(file):
     resp = requests.post(api_url, files={"audio_file": file})
-    return resp.content
+    return resp.json()
--- client/gradio/main.py
+++ client/gradio/main.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 import diarization as d
 from PIL import Image
 
-
 # DIARIZATION_API = "localho"
 
 
@@ -13,7 +12,30 @@ from PIL import Image
 # diarization_api = os.getenv('')
 
 
+def parse_diarize_result(diarize_result):
+    print(diarize_result)
+    lines = []
+    for row in diarize_result:
+        line = f'گوینده {row["speaker"]} از {row["start"]} تا {row["end"]}:\n{row["text"]}'
+        lines.append(line)
+    parsed = '\n\n'.join(lines)
+    return parsed
+
+
+
 def handle_process_btn(recorded, uploaded):
+    if recorded:
+        audio_path = recorded
+    elif uploaded:
+        audio_path = uploaded
+    else:
+        return "record or upload an audio file"
+    audio_file = open(audio_path, "rb")
+    result = d.diarize(audio_file)
+    return parse_diarize_result(result)
+
+
+def handle_process_btn_image(recorded, uploaded):
     if recorded:
         audio_path = recorded
     elif uploaded:
@@ -37,7 +59,8 @@ with gr.Blocks() as demo:
             source="microphone", type="filepath", label="record")
         uploaded_audio = gr.Audio(type="filepath", label="from drive")
     process_btn = gr.Button("Process")
-    output = gr.Image(type="pil", label="result")
+    # output = gr.Image(type="pil", label="result")
+    output = gr.Textbox(label="result")
     process_btn.click(fn=handle_process_btn, inputs=[
                       recorded_audio, uploaded_audio], outputs=output, api_name="process")
 
